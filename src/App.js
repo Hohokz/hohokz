5a4225741f960cbc28fc1889de3ecb928c8e9eb6
hohokz <hohokz@hotmail.com>
2022-09-29 01:48:14 +0700

    feat : add nav and exsimple content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './componect/header';
+import Content from './componect/content';
 
 function App() {
   return (
-   
-  );
+    <>
+
+    <Header/>
+    <Content/>
+
+    
+    </>
+  )
 }
 
 export default App;
---
+++ src/componect/content.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Content() {
+    return (
+        <div className="z-0 mt-[-80px] w-screen flex justify-center">
+            <div className="">
+                <img src="images/home_screen_full.jpg" width="1280 px" alt="images" />
+                <p className="pl-[20px] text-[60px]  ">“In photography , the smallest thing can be</p>
+                <p className="pl-[20px] text-[60px]">a great subject. The little, human detail can</p>
+                <p className="pl-[20px] text-[60px] line-through"> become a Leitmotiv.” </p>
+                <p className="pl-[20px] text-[90px] text-right">Henri Cartier-Bresson</p>
+            </div>
+        </div>
+    )
+}
+
+export default Content;
