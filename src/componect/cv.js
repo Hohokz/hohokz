57519f768424781c3634d41522af41160da7fbd4
hohokz <hohokz@hotmail.com>
2022-09-29 18:39:11 +0700

    fix more text

--- src/componect/cv.js
+++ src/componect/cv.js
@@ -16,7 +16,10 @@ function Cv() {
                             Photographer / Flimmaker / Software Development in Training
                         </p>
                         <p className="ml-[20px] font-thin tracking-widest text-xs text-gray-800">
-                            Bron 1990 in ChiangMai / Live and Learn in ChiangMai / Now ! I'm Software Development in Training
+                            Bron 1990 in ChiangMai 
+                        </p>
+                        <p className="ml-[20px] font-thin tracking-widest text-xs text-gray-800">
+                            Live and Learn in ChiangMai / Now ! I'm Software Development in Training
                         </p>
                         <p className="mt-[50px] mr-[45px] flex justify-center font-thin text-right tracking-widest text-lx text-black">
                             Educate
