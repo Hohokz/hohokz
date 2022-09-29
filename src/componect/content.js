8fcbeafd3de1708b9deb3d8154ba158428d5de52
hohokz <hohokz@hotmail.com>
2022-09-29 14:34:18 +0700

    feat : reset new

--- src/componect/content.js
+++ src/componect/content.js
@@ -2,23 +2,10 @@ import React from "react";
 
 function Content() {
     return (
-        <body className="m-[20px] flex  w-screen justify-center">
-            <div className="items-center z-1 ">
-                <img className="" src="images/home_screen_full.jpg" width="1280 px" alt="images" />
-                <div className="block z-5 bg-white ">
+        <div className="m-[20px] flex  w-screen justify-center">
+                <img className="p-5" src="images/home_screen_1.jpg" width="1280 px" alt="images" />
 
-                    <p className="text-[60px] text-justify">“IN PHOTOGRAPHY , THE SMALLEST THING CAN </p>
-                    <p className="text-[60px] text-justify"> BE A GREAT SUBJECT. THE LITTLE, HUMAN DETAIL </p>
-                    <p className="text-[60px] text-justify line-through"> CAN BECONE A EITMOTIV.” </p>
-                    <p className="text-[90px] text-right">Henri Cartier-Bresson</p>
-
-                </div>
-                <div className="w-auto h-96 bg-orange-100 ">
-
-                </div>
-
-            </div>
-        </body>
+        </div>
     )
 }
 
