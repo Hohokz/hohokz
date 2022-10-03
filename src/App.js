dff91400329fd07462e0018f5cd7c25980022f2f
hohokz <hohokz@hotmail.com>
2022-10-03 22:18:25 +0700

    feat: finish everyday and withher

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Homepage from './componect/homepage';
 import Cv from './componect/cv';
 import Everyday from './componect/everyday';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Withher from './componect/withher';
 
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
           <Route path='/' element={<Homepage/>}/>
           <Route path='/cv' element={<Cv/>}/>
           <Route path='/everyday' element={<Everyday/>}/>
+          <Route path='/withher' element={<Withher/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/componect/athing.js
+++ src/componect/athing.js
@@ -1,49 +1,233 @@
 function Athing() {
     return (
-        <div className="w-screen bg-gray-50">
+        <div className="pt-[50px] w-screen bg-gray-50">
+            <h2 className="p-[20px] flex justify-center text-lx tracking-widest ">personality</h2>
             <div className=" flex flex-row justify-center ">
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-52.jpg" width="280px" />
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-78.jpg" width="280px" />
-
-
-            </div>
-            <div className=" flex flex-row justify-center ">
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-69.jpg" width="280px" />
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-75.jpg" width="280px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-4.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-9.jpg" width="400px" />
             </div>
             <div className=" flex flex-row justify-center ">
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-136.jpg" width="280px" />
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-99.jpg" width="280px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-90.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-87.jpg" width="400px" />
             </div>
             <div className=" flex flex-row justify-center ">
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-137.jpg" width="280px" />
-
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-68.jpg" width="280px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-82.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-85.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center ">
+                <img className="m-[3px]" src="./images/everyday/personality/_-97.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-95.jpg" width="400px" />
             </div>
             <div className=" flex flex-row justify-center">
 
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-113.jpg" width="280px" />
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-84.jpg" width="280px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-103.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-104.jpg" width="400px" />
             </div>
             <div className=" flex flex-row justify-center">
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-94.jpg" width="280px" />
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-21.jpg" width="280px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-121.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-112.jpg" width="400px" />
             </div>
             <div className=" flex flex-row justify-center">
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-19.jpg" width="280px" />
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-132.jpg" width="280px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-109.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-108.jpg" width="400px" />
             </div>
             <div className=" flex flex-row justify-center">
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-22.jpg" width="280px" />
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-11.jpg" width="280px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-110.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-105.jpg" width="400px" />
             </div>
             <div className=" flex flex-row justify-center">
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-73.jpg" width="280px" />
-                <img className="m-[3px]" src="./images/everyday/(a)thing/_-57.jpg" width="280px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-121.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-98.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-102.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-80.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-81.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-78.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-75.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-73.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-72.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-62.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-66.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-70.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-61.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-58.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-57.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-60.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-47.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-44.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-42.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-30.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-38.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-37.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-39.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-54.jpg" width="400px" />
             </div>
 
-        </div>
-    )
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-26.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-27.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-31.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-25.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-33.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-10.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-9.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-16.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-17.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-15.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-8.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-4.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-5.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-7.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-1.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-6.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/personality/_-2.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/personality/_-12.jpg" width="400px" />
+            </div>
+            <h2 className="p-[20px] flex justify-center text-lx tracking-widest ">koror</h2>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/koror/_-63.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/koror/_-91.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/koror/_-84.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/koror/_-49.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/koror/_-89.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/koror/_-79.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/koror/_-86.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/koror/_-111.jpg" width="400px" />
+            </div>
+            <h2 className="p-[20px] flex justify-center text-lx tracking-widest ">place</h2>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/place/_-24.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/place/_-93.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/place/_-59.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/place/_-99.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/place/_-69.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/place/_-56.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/place/_-106.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/place/_-113.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/place/_-115.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/place/_-51.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/place/_-50.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/place/_-52.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/place/_-32.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/place/_-40.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/place/_-29.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/place/_-55.jpg" width="400px" />
+            </div>
+            <h2 className="p-[20px] flex justify-center text-lx tracking-widest ">place</h2>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/r-light/_-23.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/r-light/_-34.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/r-light/_-13.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/r-light/_-14.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/r-light/_-43.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/r-light/_-35.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/r-light/_-23.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/r-light/_-34.jpg" width="400px" />
+            </div>
+            <h2 className="p-[20px] flex justify-center text-lx tracking-widest ">(a)thing</h2>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-3.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-21.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-76.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-101.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-20.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-11.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-120.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-71.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-68.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-48.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-116.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-64.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-83.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-88.jpg" width="400px" />
+            </div>
+
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-36.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-18.jpg" width="400px" />
+            </div>
+            <div className=" flex flex-row justify-center">
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-65.jpg" width="400px" />
+                <img className="m-[3px]" src="./images/everyday/(a)thing/_-67.jpg" width="400px" />
+            </div>
+
+            </div>
+            )
 }
 
-export default Athing;
+            export default Athing;
