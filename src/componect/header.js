0f52ae88b46171364a623bb14bd21019f7a74a25
hohokz <hohokz@hotmail.com>
2022-10-03 23:36:41 +0700

    finish2

--- src/componect/header.js
+++ src/componect/header.js
@@ -73,9 +73,9 @@ function Header() {
                 <div className="">
                     <div className="static flex justify-between sm:flex-col  ">
                         <div className="">
-                            <p className="ml-[50px] mt-[30px] tracking-widest font-thin text-4xl cursor-pointer hover:text-gray-400 xl:ml-[10px] xl:mt-[3px] xl:text-lg" onClick={() => navigate('/')}>
+                            <p className="ml-[50px] mt-[20px] tracking-widest font-thin text-4xl cursor-pointer hover:text-gray-400 md:ml-[10px] md:mt-[3px] md:text-lg" onClick={() => navigate('/')}>
                                 APIWAT SINGHARACH /
-                                <span className=" tracking-widest text-orange-600 hover:text-white xl:ml-[10px] xl:mt-[10px] xl:text-lg">
+                                <span className=" tracking-widest text-orange-600 hover:text-white md:ml-[10px] md:mt-[10px] md:text-lg">
                                 &ensp; HOHOKZ
                                 </span>
                             </p>
@@ -83,8 +83,8 @@ function Header() {
                         </div>
                         <div className="mt-[40px] mr-[20px]  sm:mt-[5px] sm:ml-[10px]">
                             <div>
-                                <span className="tracking-widest cursor-pointer hover:text-orange-600 sm:tracking-tight sm:text-sm" onClick={() => navigate(`/everyday`)}>EVERYDAY &nbsp;</span>
-                                <span className="tracking-widest cursor-pointer hover:text-orange-600 sm:tracking-tight sm:text-sm" onClick={() => navigate(`/withher`)}>WITH / HER &nbsp;</span>
+                                <span className="tracking-widest cursor-pointer hover:text-orange-600 sm:tracking-tight sm:text-sm" onClick={() => navigate(`/everyday`)}>EVERYDAY / &nbsp;</span>
+                                <span className="tracking-widest cursor-pointer hover:text-orange-600 sm:tracking-tight sm:text-sm" onClick={() => navigate(`/withher`)}>WITH HER / &nbsp;</span>
                                 <span className="tracking-widest cursor-pointer hover:text-orange-600 sm:tracking-tight sm:text-sm " onClick={() => navigate(`/cv`)}>BIO</span>
                             </div>
                         </div>
